191578a215e5e8ab582d5af60aafe4bb25d5c4c4
Mariam Moamen <mar@xtnd.net>
2024-12-03 20:59:11 +0200

    Selenium basics part 1

--- src/test/java/QaCartCousre/Basics.java
+++ src/test/java/QaCartCousre/Basics.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class ChangeScreenSize {
+public class Basics {
     WebDriver driver;
     @Test
-    public void setUp() {
+    public void basics() {
         driver = new ChromeDriver();
         //Open Html using selenium in chrome => locate the file in project and use "file://absolute path"
         driver.get("file:///home/mariam/Work/Learning/Testing/Automation_Rania_Mokhtar/Java/shopping_website/src/test/java/QaCartCousre/index.html");
 
+        driver.manage().window().maximize();
         //Dimension class makes control of resizing screens and it takes (width and height) of the size we want
         Dimension iphoneSE = new Dimension(375,667);
         driver.manage().window().setSize(iphoneSE);
@@ -24,6 +25,13 @@ public class ChangeScreenSize {
         //Get title in browser tab
         String title = driver.getTitle();
         System.out.println("The Title is " + title);
+
+        //Get page source
+        String pageSource = driver.getPageSource();
+        System.out.println("The page source is " + pageSource );
+
+        //The same exactly as get("")
+        driver.navigate().to("https://www.youtube.com/watch?v=qJQnFeXiKxI&list=PLJ2FoWouEU2w8wnsblKVolmhujkOWgkjo&index=19");
     }
 
 }
