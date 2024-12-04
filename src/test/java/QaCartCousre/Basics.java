e345128b8fef217d765d7e783c3e7662d9388e62
Mariam Moamen <mar@xtnd.net>
2024-12-04 21:03:49 +0200

    Selenium basics find element by (TagName , LinkText,Compound Class)

--- src/test/java/QaCartCousre/Basics.java
+++ src/test/java/QaCartCousre/Basics.java
@@ -1,5 +1,6 @@
 package QaCartCousre;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +10,7 @@ import java.util.Objects;
 
 public class Basics {
     WebDriver driver;
+
     @Test
     public void basics() {
         driver = new ChromeDriver();
@@ -16,8 +18,20 @@ public class Basics {
         driver.get("file:///home/mariam/Work/Learning/Testing/Automation_Rania_Mokhtar/Java/shopping_website/src/test/java/QaCartCousre/index.html");
 
         driver.manage().window().maximize();
+
+         String className = driver.findElement(By.className("list1")).getText();
+        System.out.println(className);
+
+        //When i use tag name it will get the first tag because there will be a lot of element with same tag name
+        String tag = driver.findElement(By.tagName("li")).getText();
+        System.out.println(tag);
+
+        //To get element when its tag is <a and contain link >
+        String text = driver.findElement(By.linkText("Go to About Page")).getText();
+        System.out.println(text);
+
         //Dimension class makes control of resizing screens and it takes (width and height) of the size we want
-        Dimension iphoneSE = new Dimension(375,667);
+        Dimension iphoneSE = new Dimension(375, 667);
         driver.manage().window().setSize(iphoneSE);
 
         //Get current url of website
@@ -30,7 +44,7 @@ public class Basics {
 
         //Get page source
         String pageSource = driver.getPageSource();
-        System.out.println("The page source is " + pageSource );
+        System.out.println("The page source is " + pageSource);
 
         //The same exactly as get("")
         driver.navigate().to("https://www.youtube.com/watch?v=qJQnFeXiKxI&list=PLJ2FoWouEU2w8wnsblKVolmhujkOWgkjo&index=19");
